187746ddf3295dc2c3170b4726a0e90efda4c3e4
jakewalter <jakeiwalter@gmail.com>
2020-06-09 12:05:57 -0500

    make a pip package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easyQuake",
+    version="0.1",
+    author="Jake Walter",
+    author_email="jakeiwalter@gmail.com",
+    description="Simplified machine-learning driven earthquake detection, location, and analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jakewalter/easyQuake",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
