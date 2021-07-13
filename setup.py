8776381699dc76fb604fa2e2276e9eb09746b339
jakewalter <jakeiwalter@gmail.com>
2021-07-12 20:30:58 -0500

    update version for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easyQuake",
-    version="0.7.0",
+    version="0.7.5",
     author="Jake Walter",
     author_email="jakeiwalter@gmail.com",
     description="Simplified machine-learning driven earthquake detection, location, and analysis",
