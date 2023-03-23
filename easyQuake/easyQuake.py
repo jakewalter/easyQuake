0d02fe8e8f3e08aacf185f095c3efa78ea2bc8a0
José Luis Sánchez-Roldán <104619963+JLSanchezRoldan@users.noreply.github.com>
2023-03-23 18:21:33 +0100

    Update easyQuake.py

--- easyQuake/easyQuake.py
+++ easyQuake/easyQuake.py
@@ -1352,7 +1352,7 @@ def cut_event_waveforms(catalog=None, project_folder=None, length=120, filteryes
                 pick.waveform_id.channel_code = pick.waveform_id.channel_code[0:-1] + "1"
               elif pick.waveform_id.channel_code[-1]=="N": 
                 pick.waveform_id.channel_code = pick.waveform_id.channel_code[0:-1] + "2"
-            st1 += read(project_folder+'/'+strday+'/'+pick.waveform_id.network_code+'.'+pick.waveform_id.station_code+'*'+pick.waveform_id.channel_code+'*mseed')
+              st1 += read(project_folder+'/'+strday+'/'+pick.waveform_id.network_code+'.'+pick.waveform_id.station_code+'*'+pick.waveform_id.channel_code+'*mseed')
             #arrivals.append(arrv)
             picks.append(pick.phase_hint)
             picktimes.append(pick.time)
